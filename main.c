3902e507ce06f55007d3ddb69266cfecfca48708
Denis Matveev <matveev@iskrauraltel.ru>
2017-08-30 13:07:40 +0500

    using epoll() now

--- main.c
+++ main.c
@@ -5,105 +5,189 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include<arpa/inet.h>
+#include <arpa/inet.h>
+#include <limits.h>
+#include <sys/epoll.h>
+#include <fcntl.h>
+#include<errno.h>
 //TODO web server logs to write
 //TODO run as daemon
 //TODO multithread or multiprocess
 //TODO config
 //TODO parse request and make response
 #define LREQUEST 1024
-int main(int argc, char** argv)
+
+
+void process_request(int d, struct sockaddr_in * cli_addr)
 {
   socklen_t clilen;
-  int sockfd,newsockfd,n_bytes_read,r;
-  char* inetaddr="0.0.0.0";
-  int portno=80;
-  struct sockaddr_in serv_addr, cli_addr;
-  size_t len_err_response;
-  clilen=sizeof(cli_addr);
-  char haddr[LREQUEST];
-  serv_addr.sin_port=htons(portno);
-  serv_addr.sin_family=AF_INET;
   char req_in[LREQUEST]; // for incoming requests
   char *message="Your IP address is: ";
   char response[LREQUEST];
   char *request="GET / HTTP/1.1";
-
+  int n_bytes_read;
   char *response_err="<html><body><center><h1>400 Bad Request</h1></center></body></html>\n";
   char *opentags="<html><body><center><h1>";
   char *closetags="</center></body></html>";
+  size_t len_err_response;
+  char haddr[INET_ADDRSTRLEN];
 
-  r=inet_pton(AF_INET,inetaddr,&serv_addr.sin_addr.s_addr);
-  if(!r)
+  len_err_response=strlen(response_err);
+
+  memset(req_in,0,LREQUEST);// filling by zeroes
+  clilen=sizeof(cli_addr);
+  n_bytes_read=read(d,req_in,LREQUEST-1);
+
+  if(n_bytes_read < 0)
+  {
+    perror("Can't read from socket");
+    if(errno == ECONNRESET) // connection reset
     {
-      fprintf(stderr,"Internet address is not valid\n");
-      fprintf(stderr,"Using 0.0.0.0\n");
-      inetaddr="0.0.0.0";
-      if((r=inet_pton(AF_INET,inetaddr,&serv_addr.sin_addr.s_addr) < 0))
-        {
-
-          perror("An error occured in address covert from human readable to machine format");
-          exit(2);
-
-        }
+        close(d);
+        return;
     }
+  }
+  if(n_bytes_read == 0) //connection closed by remote
+  {
+      close(d);
+      return;
+  }
+  else;
+  if(!(strncasecmp(req_in,request,strlen(request))))
+  {
+//    inet_ntop(AF_INET,&cli_addr.sin_addr,haddr,LREQUEST);
+    inet_ntop(AF_INET,&(cli_addr->sin_addr),haddr,INET_ADDRSTRLEN);
+    snprintf(response,LREQUEST,"%s%s%s%s\n",opentags,message,haddr,closetags);
+    write(d, response,strlen(response));
+  }
+  else
+    write(d, response_err, len_err_response);
+
+  return;
+} 
+static int setnonblocking (int sfd)
+{
+  int flags, s;
+
+  flags = fcntl (sfd, F_GETFL, 0);
+  if (flags == -1)
+    {
+      perror ("fcntl");
+      return -1;
+    }
+
+  flags |= O_NONBLOCK;
+  s = fcntl (sfd, F_SETFL, flags);
+  if (s == -1)
+    {
+      perror ("fcntl");
+      return -1;
+    }
+
+  return 0;
+}
+
+
+int main(int argc, char** argv)
+{
+  socklen_t clilen;
+  int sockfd,newsockfd,r, conn_sock, nfds, epollfd, n;
+  char* inetaddr="0.0.0.0";
+  int portno=80;
+  struct sockaddr_in serv_addr, cli_addr;
+  serv_addr.sin_port=htons(portno);
+  serv_addr.sin_family=AF_INET;
+  #define MAX_EVENTS 10
+  struct epoll_event ev, events[MAX_EVENTS];
+  const int on = 1;
+
+
+
+  if(!(r=inet_pton(AF_INET,inetaddr,&serv_addr.sin_addr.s_addr)))
+    perror("init_pton");
   else if(r < 0)
     {
-      perror("An error occured in address covert from human readable to machine format");
-      exit(2);
+      perror("An error occured in address convertion from human readable to machine format");
+      exit(EXIT_FAILURE);
     }
   else ;
 
 
-  memset(&serv_addr.sin_zero,0,8);// the same as bzero(). Stevens recommends
-  memset(req_in,0,LREQUEST);// filling by zeroes
+  memset(&serv_addr.sin_zero,0,8);
 
-  len_err_response=strlen(response_err);
 
   if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0 )
     {
-      perror("Error opening socket\n");
-      exit(1);
+      perror("Error opening socket");
+      exit(EXIT_FAILURE);
     }
+  if( setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on))) // use it before bind(). It provides immediately server restart before TIME_WAIT is expired
+     perror("Error calling setsockopt");
   if((bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0))
     {
       perror("Cannot bind socket");
-      exit(2);
+      exit(EXIT_FAILURE);
     }
 
-
+ // setnonblocking(sockfd);
   if((listen(sockfd, 255)) < 0)
     {
       perror("Can't Listen socket");
-      exit(2);
+      exit(EXIT_FAILURE);
+    }
+   epollfd = epoll_create1(0);
+   if (epollfd == -1) 
+   {
+	   perror("epoll_create1");
+	   exit(EXIT_FAILURE);
+   }
+   ev.events = EPOLLIN;
+   ev.data.fd = sockfd;
+
+
+   if (epoll_ctl(epollfd, EPOLL_CTL_ADD, sockfd, &ev) == -1) 
+   {
+       perror("epoll_ctl:");
+       exit(EXIT_FAILURE);
+   }
+
+   for (;;) 
+     {
+       nfds = epoll_wait(epollfd, events, MAX_EVENTS, -1);
+       if (nfds == -1) 
+       {
+           perror("epoll_wait");
+           exit(EXIT_FAILURE);
+       }
+
+       for (n = 0; n < nfds; ++n) 
+        {
+           if (events[n].data.fd == sockfd) 
+           {
+               conn_sock = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+               if (conn_sock == -1) 
+               {
+                   perror("accept");
+                   exit(EXIT_FAILURE);
+               }
+//             setnonblocking(conn_sock);//if set non-blocking mode we need to use edge-triggered epoll mode and check EAGAIN in while loop to get all incoming data. it is asynchronous mode 
+               ev.events = EPOLLIN | EPOLLPRI;
+               ev.data.fd = conn_sock;
+               if (epoll_ctl(epollfd, EPOLL_CTL_ADD, conn_sock,&ev) == -1) 
+               {
+                   perror("epoll_ctl: conn_sock");
+                   exit(EXIT_FAILURE);
+               }
+           } 
+           else
+             {
+               
+              process_request(events[n].data.fd,&cli_addr); 
+              shutdown(events[n].data.fd, SHUT_RD); //shutdown is needed only if request for disconnect came
+             }
+       }
     }
 
-  while(1)
-    {
-      if(((newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen)) < 0))
-        {
-          perror("Can't accept socket");
-          exit(2);
-        }
-
-
-      n_bytes_read=read(newsockfd,req_in,LREQUEST-1);
-
-      if(n_bytes_read < 0)
-        perror("Can't read from socket");
-
-      if(!(strncasecmp(req_in,request,strlen(request))))
-        {
-
-          inet_ntop(AF_INET,&cli_addr.sin_addr,haddr,LREQUEST);
-          snprintf(response,LREQUEST,"%s%s%s%s\n",opentags,message,haddr,closetags);
-          write(newsockfd, response,strlen(response));
-        }
-      else
-        write(newsockfd, response_err, len_err_response);
-
-      close(newsockfd);
-    }
   close(sockfd);
 
 
