496cbfa06542bad5d72fe77140e4c65fc7b4f5ad
dissonmatveev <disson@yandex.ru>
2017-09-28 00:13:50 +0500

    jobs queue added. see TODO!

--- jobs_queue.c
+++ jobs_queue.c
@@ -1,22 +1,95 @@
 #include"jobs_queue.h"
 
-jobs_queue_t init_jobs_queue()
+jobs_queue_t* init_jobs_queue()
 {
-  jobs_queue_t jqt;
-  if((jqt=(jobs_queue_t)calloc(sizeof(jobs_queue))) == NULL)
+  jobs_queue_t *q;
+  if((q=(jobs_queue_t*)malloc(sizeof(jobs_queue_t))) == NULL)
   {  
      WriteLogPError("init_jobs_queue()");
      return NULL;
   }
+  if((q->array=(job_t**)calloc(QUEUE_SIZE_RESERVE,sizeof(job_t*))) == NULL)
+  {
+	  WriteLogPError("init_jobs_queue()");
+	  return NULL;
+  }
   
+ q->high_bound=q->low_bound=0;
+ q->array_size = QUEUE_SIZE_RESERVE;
 
- 
-
-
-
+ return q;
 
 }
-int close_jobs_queue(jobs_queue_t);
+int close_jobs_queue(jobs_queue_t *q)
+{
+	q->low_bound=q->high_bound=q->array_size=0;
+	free(q->array);
+	free(q);
 
-int push_job(jobs_queue_t, job_t*);
-job_t* pop_job(jobs_queue_t);
+	return 0;
+}
+
+int push_job(jobs_queue_t *q, job_t *j)
+{
+     job_t **ptr;
+
+    if(q->high_bound < q->array_size)
+    {
+        q->array[(q->high_bound)++]=j;
+
+        return 0;
+    }
+    else if(q->high_bound == q->array_size)
+    {
+        ptr=(job_t**)realloc(q->array,(q->array_size)*sizeof(job_t*) + QUEUE_SIZE_RESERVE*sizeof(job_t*));
+
+        if(ptr == NULL)
+        {
+            WriteLogPError("push_job()");
+            return 2;
+        }
+        q->array[(q->high_bound)++]=j;
+        q->array_size += QUEUE_SIZE_RESERVE;
+
+        return 0;
+    }
+    else
+        return 1;
+}
+int pop_job(jobs_queue_t* q, job_t *j)
+{
+    int i, size;
+    job_t **ptr;
+    if(q->low_bound == q->high_bound)
+        return 3;
+
+    if(q->low_bound < QUEUE_SIZE_RESERVE)
+    {
+        j=q->array[(q->low_bound)++];
+
+        return 0;
+    }
+    else if(q->low_bound >= QUEUE_SIZE_RESERVE)
+    {
+        size = (q->high_bound) - (q->low_bound);//size of the queue
+        //moving all data to zero level and reset the low_bound to null
+        for(i=0; i < size; i++)
+            q->array[i]=q->array[(q->low_bound)++];
+        q->low_bound=0;//reset low_bound
+        ptr=((job_t**)realloc(q->array,size*sizeof(job_t*)));
+        if(ptr == NULL)
+		{
+			WriteLogPError("pop_job()");
+            return 2;
+		}
+        q->high_bound=size;//set high_bound to size
+        q->array_size=size;
+        j=q->array[(q->low_bound)++];
+
+        return 0;
+    }
+    else
+        return 1;
+
+}
+//TODO make all possible checks in pop() and push() actions
--- jobs_queue.h
+++ jobs_queue.h
@@ -1,29 +1,32 @@
 #ifndef JOBS_QUEUE
 #define JOBS_QUEUE
 #include <malloc.h>
-#define QUEUE_SIZE_RESERVE 1024
+#include"http.h"
+#define QUEUE_SIZE_RESERVE 102400 
 typedef struct __job
 {
-    *http_request_t req;
+    http_request_t *req;
 } job;
 
 typedef job job_t;
 
 typedef struct __jobs_queue
 {
-   size_t queue_size;
-   job_t *array;
+   job_t **array;
    size_t array_size;
    size_t high_bound;
    size_t low_bound;
 
 } jobs_queue;
-typedef jobs_queue* jobs_queue_t;
-jobs_queue_t init_jobs_queue();
-int close_jobs_queue(jobs_queue_t);
+typedef jobs_queue jobs_queue_t;
 
-int push_job(jobs_queue_t, job_t*);
-job_t* pop_job(jobs_queue_t);
+// functions prototypes 
+
+jobs_queue_t* init_jobs_queue();
+int close_jobs_queue(jobs_queue_t*);
+
+int push_job(jobs_queue_t*, job_t*);
+int pop_job(jobs_queue_t*, job_t*);
 
 
 #endif
