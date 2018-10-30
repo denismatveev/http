644a09a62bde9c48f56ba14c7d4fcbfbd46ea9f8
dissonmatveev <disson@yandex.ru>
2018-10-31 00:53:29 +0500

    deleted unnecessary files

--- httpresponseheader.c
+++
@@ -1,8 +0,0 @@
-#include"httpresponseheader.h"
-#include<time.h>
-void HTTPResponseDate(httpResponseHeader* header)
-{
-    time_t result = time(NULL);
-    char date[96] =  asctime (result);
-    strncmp(date, header->date, 96);
-}
