662218cb9351d78b5f0d6ae3a26c126de01acbdb
root <root@linux.local>
2021-01-11 19:26:51 +0500

    fixed an error with pointer

--- main.c
+++ main.c
@@ -43,35 +43,35 @@ int main(int argc, char** argv)
     if((create_config(&cfg, cfgFile)))
         printf("Something went wrong. Check logs\n");
 
-//    pid=fork();//child process
+    pid=fork();//child process
 
-//    if(pid == -1)
-//    {
-//        WriteLogPError("Error starting daemon");
-//        exit(EXIT_FAILURE);
-//    }
-//    if(pid)
-//    {
-//        WriteLog("Started OK, My PID = %i", pid);
-//        exit(EXIT_SUCCESS);
-//    }
-//    /* the following code is executing in child process */
-//    if((setsid()) < 0)
-//    {
-//        WriteLog("An Error occured. Stop");
-//        exit(EXIT_FAILURE);
-//    }
-//    umask(0);
+    if(pid == -1)
+    {
+        WriteLogPError("Error starting daemon");
+        exit(EXIT_FAILURE);
+    }
+    if(pid)
+    {
+        WriteLog("Started OK, My PID = %i", pid);
+        exit(EXIT_SUCCESS);
+    }
+    /* the following code is executing in child process */
+    if((setsid()) < 0)
+    {
+        WriteLog("An Error occured. Stop");
+        exit(EXIT_FAILURE);
+    }
+    umask(0);
 
-//    if((chdir("/")) < 0)
-//    {
-//        WriteLog("Can't change directory");
-//        exit(EXIT_FAILURE);
-//    }
+    if((chdir("/")) < 0)
+    {
+        WriteLog("Can't change directory");
+        exit(EXIT_FAILURE);
+    }
 
-//    fclose(stderr);
-//    fclose(stdin);
-//    fclose(stdout);
+    fclose(stderr);
+    fclose(stdin);
+    fclose(stdout);
 
 
     sigemptyset(&set);
